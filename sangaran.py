1ea86db2fa85d01dbed2233120efa3984f219dfe
VSankaran25 <134500948+VSankaran25@users.noreply.github.com>
2023-05-24 16:37:53 +0530

    project 1

---
+++ sangaran.py
@@ -0,0 +1,13 @@
+print('hello world!')
+person=input ('what is your name?')
+print('hello',person)
+
+age=14
+print('i am',age,' years old')
+14
+weight=75
+print('my weight is ',weight,' kg')
+75
+height=173
+print('my height is ',height,' cm')
+173
